66a6e4e96961c4e4e0983aa8df092d3815dbc25e
quantumetrica <isaac.macedo@quantumetrica.com>
2024-11-04 20:03:34 -0300

    Aula 36. Install PostgreSQL database adaptor [Follow Along]

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,15 +8,15 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
-    
+
     def test_add_number(self):
         """Test adding numbers."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-        
+
     def test_subtract_number(self):
         """Test substracting numbers."""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
