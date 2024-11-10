67c4d8cc9908e39414bcb03e5ac2da9109874eea
quantumetrica <isaac.macedo@quantumetrica.com>
2024-11-10 18:35:26 -0300

    43. Update Docker Compose and CI/CD

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -22,7 +22,7 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_for_db_delay(self, patched_sleep, patched_check):
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting Operationalerror.
         Roda 6 vezes e retorna True no final"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
