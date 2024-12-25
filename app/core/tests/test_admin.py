f910efa7c60599493f02b5389ef0e48f0de62eaa
isaacylm <isaacyvesl@gmail.com>
2024-12-25 18:34:45 -0300

    aula 57

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -13,7 +13,7 @@ class AdminSiteTests(TestCase):
     def setUp(self):
         """Create user and client."""
         self.client = Client()
-        self.admin_user = get_user_model.create_superuser(
+        self.admin_user = get_user_model().objects.create_superuser(
             email='admin@example.com',
             password='testpass123',
         )
@@ -31,3 +31,10 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=(self.user.id))
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200) 
